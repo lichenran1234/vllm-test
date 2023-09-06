236578e3e8ec3b61dfac7b0ff8618d7f91177896
Chenran Li <chenranli657@gmail.com>
2023-09-05 22:18:55 -0700

    Update vllm_uvicorn_server.py

--- vllm_uvicorn_server.py
+++ vllm_uvicorn_server.py
@@ -18,20 +18,21 @@ from input_data_parser import read_input_data
 
 SUPPORTED_SAMPLING_PARAMS = ["presence_penalty", "frequency_penalty", "temperature", "top_p", "top_k", "stop", "max_tokens"]
 
-
 app = FastAPI()
 
 
 # Use `async def` rather than `def` so this function will be executed in the main
 # event loop (together with model invocation requests). Therefore, "OK" response
-# from this endpoint indicates the main event loop is healthy.
+# from this endpoint indicates the main event loop is healthy. "Main event loop
+# being healthy" is the best approximation of "server being healthy".
 @app.get("/v2/health/live")
 async def live():
     return "OK"
 
 # Use `async def` rather than `def` so this function will be executed in the main
 # event loop (together with model invocation requests). Therefore, "OK" response
-# from this endpoint indicates the main event loop is healthy.
+# from this endpoint indicates the main event loop is healthy. "Main event loop
+# # being healthy" is the best approximation of "server being healthy".
 @app.get("/v2/health/ready")
 async def ready():
     return "OK"
@@ -39,38 +40,40 @@ async def ready():
 @app.post("/invocations")
 async def generate(request: Request) -> Response:
     request_body = await request.body()
-    
+
     try:
+        # TODO: consider moving this pre-processing logic to an external thread pool
+        # or process pool if it's taking too much time.
         model_input = read_input_data(request_body)
-        
+
         if isinstance(model_input, pd.DataFrame):
             model_input = model_input.to_dict(orient="series")
         for key, value in model_input.items():
             # Only take the first value and silently drop the rest. This means
             # only the first prompt takes effect in each HTTP request.
             model_input[key] = value.tolist()[0]
-        
+
         if "prompt" not in model_input:
             raise Exception("Invalid input. Model input missing required field 'prompt'.")
         prompt = model_input.pop("prompt")
-        
+
         sampling_params = {}
         sampling_params["n"] = model_input.pop("candidate_count", 1)
         for p in SUPPORTED_SAMPLING_PARAMS:
             if p in model_input:
                 sampling_params[p] = model_input.pop(p)
-        
+
         if model_input:
             raise Exception(
                 f"Invalid input. Model input contains unexpected parameters {list(model_input.keys())}."
                 f" Only these parameters are supported: {SUPPORTED_SAMPLING_PARAMS + ['prompt', 'candidate_count']}."
             )
-        
+
         # Following are mostly copied from https://github.com/vllm-project/vllm/blob/main/vllm/entrypoints/api_server.py
         sampling_params = SamplingParams(**sampling_params)
         request_id = random_uuid()
         results_generator = engine.generate(prompt, sampling_params, request_id)
-        
+
         # Streaming not supported yet
         # async def stream_results() -> AsyncGenerator[bytes, None]:
         #     async for request_output in results_generator:
@@ -89,7 +92,7 @@ async def generate(request: Request) -> Response:
         #     # Abort the request if the client disconnects.
         #     background_tasks.add_task(abort_request)
         #     return StreamingResponse(stream_results(), background=background_tasks)
-        
+
         # Non-streaming case
         final_output = None
         async for request_output in results_generator:
@@ -100,7 +103,7 @@ async def generate(request: Request) -> Response:
             final_output = request_output
         if final_output is None:
             raise Exception("Server failed to process this request.")
-        
+
         prompt_length = len(final_output.prompt_token_ids)
         candidates = []
         output_length = 0
@@ -113,10 +116,9 @@ async def generate(request: Request) -> Response:
             "total_tokens": prompt_length + output_length,
         }
         return JSONResponse({"predictions": [{"candidates": candidates, "metadata": metadata}]})
-        
+
     except Exception as ex:
         # Return all errors as 400 for now.
-        print(f"Request failed. HTTP response code: 400. Error message: '{ex}'")
         return JSONResponse(status_code=400, content={"error_message": str(ex)})
 
 
